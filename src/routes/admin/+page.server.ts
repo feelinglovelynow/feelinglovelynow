3214d3a0d2e8e4ecad7bc62ebc5e0803344d16e1
Chris Carrington <carrington.christopher@gmail.com>
2023-10-27 20:43:32 -0700

    Admin only local @ server too

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,22 +1,25 @@
 import search from '$lib/actions/search'
 import queryOrder from '$lib/dgraph/queryOrder'
 // import getProducts from '$lib/printful/getProducts'
+import { PUBLIC_ENVIRONMENT } from '$env/static/public'
 import type { Actions, PageServerLoad } from './$types'
 import serverPageCatch from '$lib/catch/serverPageCatch'
 
 
 export const load = (async () => {
   try {
-    // console.log(JSON.stringify(await getProducts()))
-    const orders = await queryOrder()
+    if (PUBLIC_ENVIRONMENT === 'local') {
+      // console.log(JSON.stringify(await getProducts()))
+      const orders = await queryOrder()
 
-    for (const order of orders) {
-      for (const orderItem of order.orderItems) {
-        if (orderItem.product) orderItem.product.primaryImage.src = (await import(`../../lib/img/store/${ orderItem.product.primaryImage.id }.${ orderItem.product.primaryImage.extension }`)).default
+      for (const order of orders) {
+        for (const orderItem of order.orderItems) {
+          if (orderItem.product) orderItem.product.primaryImage.src = (await import(`../../lib/img/store/${ orderItem.product.primaryImage.id }.${ orderItem.product.primaryImage.extension }`)).default
+        }
       }
-    }
 
-    return { orders }
+      return { orders }
+    }
   } catch (e) {
     return serverPageCatch(e)
   }
