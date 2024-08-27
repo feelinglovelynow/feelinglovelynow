27406fade6bb54aaa3c7a3318a73ae1a6dc54ad6
Chris Carrington <carrington.christopher@gmail.com>
2024-08-26 23:36:08 -0700

    specific build file paths

--- src/lib/scripts/mainBuild.sh
+++ src/lib/scripts/mainBuild.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-sudo find ace -type d -exec chmod 755 {} \; && # for directories
-sudo find ace -type d -exec chmod 666 {} \; && # for files
-npm run mainWrite &&
-npm run check &&
+
+# Set permissions for directories
+sudo find /var/www/html/feelinglovelynow/ace -type d -exec chmod 755 {} \; && \
+
+# Set permissions for files
+sudo find /var/www/html/feelinglovelynow/ace -type f -exec chmod 666 {} \; && \
+
+# Run npm scripts and build
+npm run mainWrite && \
+npm run check && \
 npx --env-file=.env vite build
